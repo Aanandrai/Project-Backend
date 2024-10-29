a7144eaefe38eb0dd19b7811248ee1a050264a85
Aanandrai <aanandrai.atwork@gmail.com>
2024-10-30 00:56:09 +0530

    five.4

--- app/controllers/seller/seller.controller.js
+++ app/controllers/seller/seller.controller.js
@@ -29,40 +29,40 @@ exports.signIn = async (req, res) => {
     const user = await User.findOne({ imei }).populate("subAdminId");
 
     if (!user) {
-      // res.send(encoding({ success: false, message: "User not found!" }));
-      res.send({ success: false, message: "User not found!" });
+      res.send(encoding({ success: false, message: "User not found!" }));
+      // res.send({ success: false, message: "User not found!" });
       return;
     }
 
     if (!user.isActive) {
-      // res.send(encoding({ success: false, message: "This user locked now!" }));
-      res.send({ success: false, message: "This user locked now!" });
+      res.send(encoding({ success: false, message: "This user locked now!" }));
+      // res.send({ success: false, message: "This user locked now!" });
       return;
     }
 
     if (!user.subAdminId.isActive) {
-      // res.send(
-      //   encoding({
-      //     success: false,
-      //     message: "Votre compagnie est deconnecée",
-      //   })
-      // );
-      res.send({
+      res.send(
+        encoding({
           success: false,
           message: "Votre compagnie est deconnecée",
-        }
+        })
       );
+      // res.send({
+      //     success: false,
+      //     message: "Votre compagnie est deconnecée",
+      //   }
+      // );
       return;
     }
 
     const isMatch = await bcrypt.compare(password, user.password);
     
     if (!isMatch) {
-      // res.send(
-      //   encoding({ success: false, message: "Mot de passe incorrecte" })
-      // );
+      res.send(
+        encoding({ success: false, message: "Mot de passe incorrecte" })
+      );
 
-      res.send({ success: false, message: "Mot de passe incorrecte" });
+      // res.send({ success: false, message: "Mot de passe incorrecte" });
       return;
     }
 
@@ -71,20 +71,8 @@ exports.signIn = async (req, res) => {
     });
 
     req.session.token = token;
-    // res.send(
-    //   encoding({
-    //     success: true,
-    //     token: token,
-    //     userName: user.userName,
-    //     companyName: user.subAdminId.companyName,
-    //     phoneNumber: user.subAdminId.phoneNumber,
-    //     address: user.subAdminId.address,
-    //     sellerId: user._id,
-    //     subAdminId: user.subAdminId._id,
-    //   })
-    // );
-    
-    res.send({
+    res.send(
+      encoding({
         success: true,
         token: token,
         userName: user.userName,
@@ -93,7 +81,19 @@ exports.signIn = async (req, res) => {
         address: user.subAdminId.address,
         sellerId: user._id,
         subAdminId: user.subAdminId._id,
-      });
+      })
+    );
+    
+    // res.send({
+    //     success: true,
+    //     token: token,
+    //     userName: user.userName,
+    //     companyName: user.subAdminId.companyName,
+    //     phoneNumber: user.subAdminId.phoneNumber,
+    //     address: user.subAdminId.address,
+    //     sellerId: user._id,
+    //     subAdminId: user.subAdminId._id,
+    //   });
 
   } catch (err) {
     // console.log(err);
@@ -137,18 +137,18 @@ exports.newTicket = async (req, res) => {
       if (!success) {
         // console.log("ticket check error: ", error);
         
-        // return res.send(
-        //   encoding({
-        //     success: false,
-        //     message: "ticket save failed! try again!",
-        //   })
-        // );
         return res.send(
-          {
+          encoding({
             success: false,
             message: "ticket save failed! try again!",
-          }
+          })
         );
+        // return res.send(
+        //   {
+        //     success: false,
+        //     message: "ticket save failed! try again!",
+        //   }
+        // );
       }
 
       if (new_numbers.length > 0) {
@@ -162,85 +162,85 @@ exports.newTicket = async (req, res) => {
 
         await ticket.save((err, savedTicket) => {
           if (err) {
-            // console.log("new ticket data error :", err);
-            // res.send(
-            //   encoding({
-            //     success: false,
-            //     message: "new ticket create failed! please try again!",
-            //   })
-            // );
+            console.log("new ticket data error :", err);
             res.send(
-              {
+              encoding({
                 success: false,
                 message: "new ticket create failed! please try again!",
-              }
+              })
             );
+            // res.send(
+            //   {
+            //     success: false,
+            //     message: "new ticket create failed! please try again!",
+            //   }
+            // );
             return;
           } else {
             const newId = savedTicket.ticketId;
-            // res.send(
-            //   encoding({
-            //     success: true,
-            //     message: "success",
-            //     ticketId: newId,
-            //     numbers: new_numbers,
-            //     limit_data,
-            //     block_data,
-            //   })
-            // );
             res.send(
-             {
+              encoding({
                 success: true,
                 message: "success",
                 ticketId: newId,
                 numbers: new_numbers,
                 limit_data,
                 block_data,
-              }
+              })
             );
+            // res.send(
+            //  {
+            //     success: true,
+            //     message: "success",
+            //     ticketId: newId,
+            //     numbers: new_numbers,
+            //     limit_data,
+            //     block_data,
+            //   }
+            // );
             return;
           }
         });
       } else {
-        // res.send(
-        //   encoding({
-        //     success: true,
-        //     message: "you cant create ticket!",
-        //     numbers: new_numbers,
-        //     limit_data,
-        //     block_data,
-        //   })
-        // );
         res.send(
-         {
+          encoding({
             success: true,
             message: "you cant create ticket!",
             numbers: new_numbers,
             limit_data,
             block_data,
-          }
+          })
         );
+        // res.send(
+        //  {
+        //     success: true,
+        //     message: "you cant create ticket!",
+        //     numbers: new_numbers,
+        //     limit_data,
+        //     block_data,
+        //   }
+        // );
         return;
       }
     } else {
-      // res.send(
-      //   encoding({
-      //     success: false,
-      //     message: `Haiti local time now: ${currentTime}. \n Time is up!. Sorry you cann't create ticket.`,
-      //   })
-      // );
       res.send(
-        {
+        encoding({
           success: false,
           message: `Haiti local time now: ${currentTime}. \n Time is up!. Sorry you cann't create ticket.`,
-        }
+        })
       );
+      // res.send(
+      //   {
+      //     success: false,
+      //     message: `Haiti local time now: ${currentTime}. \n Time is up!. Sorry you cann't create ticket.`,
+      //   }
+      // );
       return;
     }
   } catch (err) {
     // console.log(err);
-    // res.status(500).send(encoding({ message: err }));
-    res.status(500).send({ message: err });
+    res.status(500).send(encoding({ message: err }));
+    // res.status(500).send({ message: err });
   }
 };
 // Read //tested
