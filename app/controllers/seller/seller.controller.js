9c14efb3f4b1297d5146aaed731e86d3e07e28bf
Aanandrai <aanandrai.atwork@gmail.com>
2024-10-30 15:40:24 +0530

    ten.6

--- app/controllers/seller/seller.controller.js
+++ app/controllers/seller/seller.controller.js
@@ -117,8 +117,8 @@ exports.newTicket = async (req, res) => {
     const { lotteryCategoryName } = req.body;
     const sellerId=req.userId
     console.log("getting the request from app")
-    // const numbers = JSON.parse(req.body.numbers);
-    const numbers = req.body.numbers;
+    const numbers = JSON.parse(req.body.numbers);
+    // const numbers = req.body.numbers;
     const today = moment().tz(haitiTimezone).format("yyyy-MM-DD");
     const currentTime = moment().tz(haitiTimezone).format("HH:mm");
 
